472e7103ec66d9f3f8103e758fcf0f355f68a080
NamitDhiman <195862020+NamitDhiman@users.noreply.github.com>
2025-01-27 11:33:04 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    double c;
+    scanf("%lf",&c);
+    printf("You entered: %lf",c);
+    return 0;
+}
