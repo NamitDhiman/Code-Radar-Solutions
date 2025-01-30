bbce37e0b11d34751d2256598fa98a1aa8549564
NamitDhiman <195862020+NamitDhiman@users.noreply.github.com>
2025-01-30 11:40:15 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -6,12 +6,12 @@ char* welcome() {
 
 int main() {
  //   printf("%s", welcome());
- int a,b;
- scanf("%d%d"&a,&b);
- if (a>0 || b>0){
-    printf("True");
- }else{
-    printf("False");
- }
+    int a,b;
+    scanf("%d%d"&a,&b);
+    if (a>0 || b>0){
+        printf("True");
+    }else{
+        printf("False");
+    }
     return 0;
 }
